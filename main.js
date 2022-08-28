5ff06f56ef9e989c1638281cac5feb85109d29a9
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-08-29 00:35:33 +0100

    Add comments to main.js file

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 if (document.getElementById("eChatWindow")) {
-	const eChatWindow = document.getElementById("eChatWindow")
-	Object.assign(eChatWindow.style, {
-		display: getComputedStyle(eChatWindow).getPropertyValue("display") == "block" ? "none" : "block",
+
+	// Find if the window already exists (has been previously closed)
+	// If it has been, just make it visible again
+	const Window = document.getElementById("eChatWindow")
+	Object.assign(Window.style, {
+		display: getComputedStyle(Window).getPropertyValue("display") == "block" ? "none" : "block",
 	})
 } else {
-	/* <div id="eChatWindow" style="all: initial; position: fixed; top: 0; left: 0; z-index: 2147483647">
-	<header id="eChatMainHeader" style="background: #0000007f; backdrop-filter: blur(6px); border-radius: 16px 16px 0 0; cursor: move; height: 40px">
-		<button style="all: initial; background: none; padding: 5px 10px 0; float: right; color: white; font-size: 28px; font-family: Arial; cursor: pointer">X</button>
-	</header>
-	<iframe src="https://localhost:4173" title="eChat" height="500" width="375" style="border: none; border-radius: 0 0 16px 16px" />
-</div> */
 
-	// Create the ui
-
-	const eChatWindow = document.createElement("div")
-	Object.assign(eChatWindow.style, {
+	// Otherwise, create the ui
+	const Window = document.createElement("div")
+	Object.assign(Window.style, {
 		all: "initial",
 		position: "fixed",
 		top: 0,
@@ -22,10 +18,11 @@ if (document.getElementById("eChatWindow")) {
 		zIndex: Number.MAX_SAFE_INTEGER,
 		display: "block",
 	})
-	eChatWindow.id = "eChatWindow"
+	Window.id = "eChatWindow"
 
-	const eChatMainHeader = document.createElement("div")
-	Object.assign(eChatMainHeader.style, {
+	// Create the header
+	const Header = document.createElement("div")
+	Object.assign(Header.style, {
 		all: "initial",
 		background: "#0000007f",
 		backdropFilter: "blur(6px)",
@@ -35,8 +32,9 @@ if (document.getElementById("eChatWindow")) {
 		display: "block",
 	})
 
-	const eChatMainHeaderCloseButton = document.createElement("button")
-	Object.assign(eChatMainHeaderCloseButton.style, {
+	// Add the close button
+	const HeaderCloseButton = document.createElement("button")
+	Object.assign(HeaderCloseButton.style, {
 		all: "initial",
 		background: "none",
 		padding: "5px 10px 0",
@@ -46,63 +44,70 @@ if (document.getElementById("eChatWindow")) {
 		fontFamily: "Arial",
 		cursor: "pointer",
 	})
-	eChatMainHeaderCloseButton.innerText = "X"
+	HeaderCloseButton.innerText = "X"
 
-	const eChatIframe = document.createElement("iframe")
-	Object.assign(eChatIframe.style, {
+	// Make the iframe, a window to the actual eChat application
+	const iframe = document.createElement("iframe")
+	Object.assign(iframe.style, {
 		border: "none",
 		borderRadius: "0 0 16px 16px",
 	})
-	eChatIframe.src = "https://chat.echatapp.ml"
-	eChatIframe.title = "eChat"
-	eChatIframe.height = "500"
-	eChatIframe.width = "375"
+	iframe.src = "https://chat.echatapp.ml"
+	iframe.title = "eChat"
+	iframe.height = "500"
+	iframe.width = "375"
 
-	eChatMainHeader.appendChild(eChatMainHeaderCloseButton)
-	eChatWindow.appendChild(eChatMainHeader)
-	eChatWindow.appendChild(eChatIframe)
-	document.body.appendChild(eChatWindow)
+	Header.appendChild(HeaderCloseButton)
+	Window.appendChild(Header)
+	Window.appendChild(iframe)
+	document.body.appendChild(Window)
 
-	eChatMainHeaderCloseButton.onclick = function () {
-		Object.assign(eChatWindow.style, {
+	// Close the window when the close button is clicked
+	HeaderCloseButton.onclick = function () {
+		Object.assign(Window.style, {
 			display: "none",
 		})
 	}
 
-	// thanks, w3schools	https://www.w3schools.com/howto/howto_js_draggable.asp
+	// Make the window draggable by the header
+	// Thanks w3schools for dragger code	https://www.w3schools.com/howto/howto_js_draggable.asp
+	Header.onmousedown = dragMouseDown
+
 	let pos1 = 0,
 		pos2 = 0,
 		pos3 = 0,
 		pos4 = 0
 
-	eChatMainHeader.onmousedown = dragMouseDown
-
 	function dragMouseDown(e) {
 		e = e || window.event
 		e.preventDefault()
-		// get the mouse cursor position at startup:
+
+		// Get the mouse cursor position at startup:
 		pos3 = e.clientX
 		pos4 = e.clientY
 		document.onmouseup = closeDragElement
-		// call a function whenever the cursor moves:
+
+		// Call a function whenever the cursor moves:
 		document.onmousemove = elementDrag
 	}
 
 	function elementDrag(e) {
 		e = e || window.event
 		e.preventDefault()
-		// calculate the new cursor position:
+
+		// Calculate the new cursor position:
 		pos1 = pos3 - e.clientX
 		pos2 = pos4 - e.clientY
 		pos3 = e.clientX
 		pos4 = e.clientY
-		// set the element's new position:
-		eChatWindow.style.top = eChatWindow.offsetTop - pos2 + "px"
-		eChatWindow.style.left = eChatWindow.offsetLeft - pos1 + "px"
+
+		// Set the element's new position:
+		Window.style.top = Window.offsetTop - pos2 + "px"
+		Window.style.left = Window.offsetLeft - pos1 + "px"
 	}
 
 	function closeDragElement() {
-		// stop moving when mouse button is released:
+		// Stop moving when mouse button is released:
 		document.onmouseup = null
 		document.onmousemove = null
 	}
